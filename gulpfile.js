76760b2c7342eb4713c81becd58d5ab75c9c0da1
Johannes Stein <johannesstein@freeze-dev.com>
2015-03-07 21:36:54 +0100

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,43 @@
 var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var babel = require('gulp-babel');
+var concat = require('gulp-concat');
+var webpack = require('gulp-webpack');
+var wrapCommonjs = require('gulp-wrap-commonjs');
+var del = require('del');
+var consolidate = require('gulp-consolidate');
 
-gulp.task('default', function() {
-  // place code for your default task here
+var paths = {
+  source: {
+    
+  },
+  build: {
+    debug: 'build/debug',
+    release: 'build/release'
+  }
+};
+
+gulp.task('clean', function() {
+  return del('build');
 });
+
+var scripts = function() {
+  return gulp.src('src/app.js')
+    //.pipe(sourcemaps.init())
+    //.pipe(babel())
+    //.pipe(wrapCommonjs())
+    .pipe(webpack(require('./webpack.config.js')))
+    //.pipe(concat('all.js'))
+    //.pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(paths.build.debug));
+};
+
+gulp.task('scripts', scripts);
+
+gulp.task('template', function() {
+  return gulp.src('templates/index.html')
+    .pipe(consolidate('handlebars'))
+    .pipe(gulp.dest(paths.build.debug));
+});
+
+gulp.task('default', ['clean'], scripts);
