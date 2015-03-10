e53cf12a1504fd63cc155229c8acf895263bdcb7
Johannes Stein <johannesstein@freeze-dev.com>
2015-03-10 19:07:07 +0100

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   target: 'web',
   devtool: '#source-map',
+  entry: './src/app.js',
   output: {
     filename: 'all.js',
     sourceMapFilename: '[file].map'
@@ -9,9 +10,12 @@ module.exports = {
     loaders: [
       {test: /\.js/, loader: 'babel-loader'}
     ],
-    noParse: /\.min\.js/
+    noParse: [/\.min\.js/, /\/node_modules\/react\/dist\/react\.js/]
   },
   resolve: {
+    alias: {
+      react: 'react/dist/react.js'
+    },
     extensions: ['', '.js', '.json', '.jsx'],
     modulesDirectories: ['node_modules']
   }
