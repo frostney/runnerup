438314593f99a5679c28ef94d5ea6fafb58cc3e6
Johannes Stein <johannesstein@freeze-dev.com>
2015-04-07 23:09:25 +0200

    Use SprintList and SprintBoard in project

--- src/scripts/components/Project.js
+++ src/scripts/components/Project.js
@@ -1,18 +1,33 @@
 'use strict';
 
 var React = require('react/addons');
+const {Grid, Row, Col} = require('react-bootstrap');
+
+var SprintList = require('./SprintList');
+var SprintBoard = require('./SprintBoard');
 
 require('styles/Project.less');
 
 var Project = React.createClass({
+  contextTypes: {
+    router: React.PropTypes.func
+  },
   render: function () {
+    const {name, sprint} = this.context.router.getCurrentParams();
+
     return (
-        <div>
-          <p>Content for Project</p>
-        </div>
-      );
+      <div>
+        <h1>{name}</h1>
+        <Grid>
+          <Row>
+            <Col xs={6} md={4}><SprintList /></Col>
+            <Col xs={12} md={8}><SprintBoard /></Col>
+          </Row>
+        </Grid>
+      </div>
+    );
   }
 });
 
-module.exports = Project; 
+module.exports = Project;
 
