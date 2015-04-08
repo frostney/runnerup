b0ba9baed0e4afb068d8e7cad478d41e012fad57
Johannes Stein <johannesstein@freeze-dev.com>
2015-04-08 19:12:58 +0200

    List all projects

--- src/scripts/components/ProjectList.js
+++ src/scripts/components/ProjectList.js
@@ -1,18 +1,23 @@
 'use strict';
 
 var React = require('react/addons');
+import AppStore from '../model/Store';
 
 require('styles/ProjectList.less');
 
 var ProjectList = React.createClass({
   render: function () {
+    const projectNames = Object.keys(AppStore.projects);
+
+    const list = projectNames.map(function(name) {
+      return <li>{name}</li>;
+    });
+
     return (
-        <div>
-          <p>Content for ProjectList</p>
-        </div>
+        <div>{list}</div>
       );
   }
 });
 
-module.exports = ProjectList; 
+module.exports = ProjectList;
 
